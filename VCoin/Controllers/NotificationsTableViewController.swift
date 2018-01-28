7e733498575f8998d9b572935b915e8d306e1346
Marcin Czachurski <mczachurski@icloud.com>
2018-01-28 15:18:16 +0100

    New views.

---
+++ VCoin/Controllers/NotificationsTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NotificationsTableViewController.swift
+//  VCoin
+//
+//  Created by Marcin Czachurski on 28.01.2018.
+//  Copyright © 2018 Marcin Czachurski. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsTableViewController: BaseTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+}
---
+++ VCoin/Controllers/WalletTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WalletTableViewController.swift
+//  VCoin
+//
+//  Created by Marcin Czachurski on 28.01.2018.
+//  Copyright © 2018 Marcin Czachurski. All rights reserved.
+//
+
+import UIKit
+
+class WalletTableViewController: BaseTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "walletitemcell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+}
